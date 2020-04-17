49fc79822b2a9d5455db99eba79f06d0e05ab34a
raqueru <raquelolho@hotmail.com>
2020-04-17 13:55:57 -0300

    Placeholder do som dos passos, Placeholder da animação de andar e do idle do personagem  e os respectivos códigos Coloquei a luz que faltava na cena do teste de andar e criei as pastas para animação e som.

--- Jogo time 2/Assets/Scripts/MovimentoPlayer.cs
+++ Jogo time 2/Assets/Scripts/MovimentoPlayer.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class MovimentoPlayer : MonoBehaviour
 {
+    private Animator animator;
+    public float StepsTime;
     public float speed;
     private Rigidbody2D rb;
     Vector2 movement;
+    private AudioSource steps;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        steps= gameObject.GetComponent<AudioSource>();
+        animator= gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,6 +33,22 @@ public class MovimentoPlayer : MonoBehaviour
 
     private void FixedUpdate()
     {
+         steps.Pause();
+         
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        if (movement.x!=0 || movement.y!=0){
+            if (movement.x <= 0)
+		    {
+			    transform.rotation = new Quaternion(0, 180, 0, 0);
+            }
+		    else if (movement.x > 0)
+		    {
+			    transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+         steps.Play();
+         animator.SetBool("IsWalking",true);
+        }
+       
+        else animator.SetBool("IsWalking",false); 
     }
 }
