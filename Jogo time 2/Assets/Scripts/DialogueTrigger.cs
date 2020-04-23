d6229edbd96c4537639554c96cb192cc344d59ae
Raphael Ponce <raphafleury123@gmail.com>
2020-04-23 09:13:33 -0300

    POC

--- Jogo time 2/Assets/Scripts/DialogueTrigger.cs
+++ Jogo time 2/Assets/Scripts/DialogueTrigger.cs
@@ -1,12 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
+
+    public Canvas dialogueCanvas;
+    private bool canActivate;
+
+    /*
     public Dialogue Dialogue;
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(Dialogue);
+    }
+    */
+
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && canActivate)
+        {
+            dialogueCanvas.gameObject.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canActivate = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canActivate = false;
+        }
     }
 }
