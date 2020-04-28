061d7e43a40ff86a462943a1e4d7218065475db8
raqueru <raquelolho@hotmail.com>
2020-04-28 16:49:55 -0300

    Implementada funcao inicial de modificador de atributos, ainda não polida

---
+++ Jogo time 2/Assets/Scripts/ModificadorAtributos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModificadorAtributos : MonoBehaviour
+{
+     public CombateUnidade NovoAtributo;
+ 
+     private CombateAtributos AtributoAtual;
+     
+   
+     public int Vida;
+    int VidaAtual;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        AtributoAtual= gameObject.GetComponent<CombateAtributos>();
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VidaAtual= AtributoAtual.getVidaAtual();
+        if (VidaAtual<=Vida){
+            ModificaAtributo(NovoAtributo);
+        }
+    }
+
+    void ModificaAtributo(CombateUnidade NovoAtributo){
+            AtributoAtual.atributos=NovoAtributo;
+            this.enabled=false;
+            
+
+    }
+}
