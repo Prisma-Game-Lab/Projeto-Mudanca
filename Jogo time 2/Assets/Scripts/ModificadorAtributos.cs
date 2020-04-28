a701b25532970eb631ef57ad7ea77f5eb8b5d370
raqueru <raquelolho@hotmail.com>
2020-04-28 20:06:27 -0300

    Funcao de modificadores em duas etapas lapidado

--- Jogo time 2/Assets/Scripts/ModificadorAtributos.cs
+++ Jogo time 2/Assets/Scripts/ModificadorAtributos.cs
@@ -4,35 +4,42 @@ using UnityEngine;
 
 public class ModificadorAtributos : MonoBehaviour
 {
-     public CombateUnidade NovoAtributo;
- 
-     private CombateAtributos AtributoAtual;
-     
-   
-     public int Vida;
-    int VidaAtual;
+    public Modificador[] NovosAtributos;
+
+    private CombateAtributos AtributoAtual;
+
+    private int i;
+
+    private int VidaAtual;
     // Start is called before the first frame update
     void Start()
     {
-       
-        AtributoAtual= gameObject.GetComponent<CombateAtributos>();
-         
+
+        AtributoAtual = gameObject.GetComponent<CombateAtributos>();
+        i = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        VidaAtual= AtributoAtual.getVidaAtual();
-        if (VidaAtual<=Vida){
-            ModificaAtributo(NovoAtributo);
-            NovoAtributo.vida=VidaAtual;
+        VidaAtual = AtributoAtual.getVidaAtual();
+
+        if (VidaAtual <= NovosAtributos[i].Vida)
+        {
+             
+                ModificaAtributo(NovosAtributos[i].Atributo);
+                NovosAtributos[i].Atributo.vida = VidaAtual;
+                i++;
+                } 
+    }
+        void ModificaAtributo(CombateUnidade NovoAtributo)
+        {
+
+            AtributoAtual.atributos = NovoAtributo;
+
+
+
+
         }
     }
-
-    void ModificaAtributo(CombateUnidade NovoAtributo){
-            AtributoAtual.atributos=NovoAtributo;
-            this.enabled=false;
-            
-
-    }
-}
