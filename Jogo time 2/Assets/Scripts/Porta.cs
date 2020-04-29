af49ec9ca257a4a9b4be57218d6430e7a7eb9380
mlhomsi <mlhomsi@gmail.com>
2020-04-29 19:24:17 -0300

    Porta

---
+++ Jogo time 2/Assets/Scripts/Porta.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porta : MonoBehaviour
+{
+    public GameObject GM;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GM.GetComponent<SceneControl>().LoadtNextScene("Festa");
+    }
+}
