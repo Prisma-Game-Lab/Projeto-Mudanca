9f63fcdbb264631a7c5c852912fef0548d82d11d
raqueru <raquelolho@hotmail.com>
2020-05-18 02:49:14 -0300

    coloquei a setinha que aparece no dialogo

---
+++ Jogo time 2/Assets/Scripts/DialogueTriangle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriangle : MonoBehaviour
+{
+        private void Start()
+    {
+        StartCoroutine(desaparecer());
+        
+    }
+
+    IEnumerator desaparecer()
+    {
+
+        while (true){
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(1);
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        yield return new WaitForSecondsRealtime(1);
+    }
+    }
+}
