3f6677f607c64a056d0ec80941fafbe3a63ed7ec
raqueru <raquelolho@hotmail.com>
2020-05-22 14:50:58 -0300

    Fiz um sistema de trigger de dialogo bem rapidinho, falta polir bastante

---
+++ Jogo time 2/Assets/Scripts/ChangeDialogue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDialogue : MonoBehaviour
+{
+
+    public DialogueBlock NewDialogue;
+    public GameObject OldDialogueObject;
+
+    private DialogueTrigger DialogueTrigger;
+    void Start()
+    {
+        DialogueTrigger = gameObject.GetComponent<DialogueTrigger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && DialogueTrigger.canActivate)
+        {
+            OldDialogueObject.GetComponent<DialogueTrigger>().Dialogue = NewDialogue;
+        }
+
+
+    }
+    }
