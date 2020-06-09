1f76cd0f07d73933c2813133f8c9a3a83f807ad7
raqueru <raquelolho@hotmail.com>
2020-06-09 17:32:57 -0300

    Cutscene da festa pronto

---
+++ Jogo time 2/Assets/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+ 
+public class Cutscenetrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    private DialogueManager dialogue;
+
+ 
+    // Use this for initialization
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+        dialogue=FindObjectOfType<DialogueManager>();
+    }
+ 
+ 
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player" &&dialogue.DialogueOn==false)
+        {
+            timeline.Play();
+        }
+    }
+}
