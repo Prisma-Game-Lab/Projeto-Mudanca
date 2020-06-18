adeb111c7693dc447ab2ee1cbfdf6a9de7125be7
raqueru <raquelolho@hotmail.com>
2020-06-18 15:41:33 -0300

    mudancas para o armario funcionar depois que se perde

--- Jogo time 2/Assets/Scripts/DialogueChangeManager.cs
+++ Jogo time 2/Assets/Scripts/DialogueChangeManager.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class DialogueChangeManager : MonoBehaviour
 {
-   
-   
+
+
     public GameObject OldDialogueObject;
 
-    // Update is called once per frame
+    private SceneControl scene;
+     
+
+    private void Start()
+    {
+        scene = GameObject.Find("GameManager").GetComponent<SceneControl>();
+    }
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown("space")) && gameObject.GetComponent< DialogueTrigger>().canActivate)
+         if (scene.derrota == true)
+            {
+                  OldDialogueObject.GetComponent<DialogueTrigger>().Dialogue[0]=OldDialogueObject.GetComponent<DialogueTrigger>().Dialogue[2];
+                  
+                  OldDialogueObject.GetComponent<DialogueTrigger>().Dialogue[1]=OldDialogueObject.GetComponent<DialogueTrigger>().Dialogue[3];
+            }
+        if ((Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown("space")) && gameObject.GetComponent<DialogueTrigger>().canActivate)
         {
-           OldDialogueObject.GetComponent<DialogueTrigger>().i=1;
+             
+            OldDialogueObject.GetComponent<DialogueTrigger>().i =1;
+            
+
         }
 
-   
-}
+
+    }
 }
