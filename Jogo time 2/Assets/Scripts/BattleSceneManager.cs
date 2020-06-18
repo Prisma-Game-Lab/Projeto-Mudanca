050323727ba841e6440fae99eb6872efb2a02efd
raqueru <raquelolho@hotmail.com>
2020-06-17 23:33:51 -0300

    Ajeitei umas paradas no quarto

---
+++ Jogo time 2/Assets/Scripts/BattleSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleSceneManager : MonoBehaviour
+{
+   
+     private SceneControl SceneController;
+     public string scene;
+     private void Awake() {
+         SceneController=FindObjectOfType<SceneControl>();
+     }
+             public void Click1(string sceneToLoad)
+           {
+                 SceneManager.LoadScene( sceneToLoad);
+           }
+}
