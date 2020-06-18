f091e7edce1065650df4143a1e52aa433a6d6728
raqueru <raquelolho@hotmail.com>
2020-06-17 23:05:34 -0300

    criei a lista de dialogos

---
+++ Jogo time 2/Assets/Scripts/ListaDialogos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListaDialogos : MonoBehaviour
+{
+    private static ListaDialogos _instance;
+    public static ListaDialogos Instance { get { return _instance; } }
+
+    public List<DialogueBlock> Listadedialogos = new List<DialogueBlock>();
+    public List<DialogueBlock> DialogosMutaveis = new List<DialogueBlock>(); 
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                _instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+           
+        }
+
+    }
+
+}
